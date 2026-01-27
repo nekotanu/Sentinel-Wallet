f44a37ba2a4adfc308e815e0e229a58e578e4abd
Aayush sharma <ayutanu1230@gmail.com>
2026-01-27 12:35:24 +0530

    chore(repo): remove legacy extension folder after project unificnation

--- EXTENSION/src/background.ts
+++
@@ -1,9 +0,0 @@
-// chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
-//   console.log("TX intercepted:", msg);
-//   sendResponse({ allow: true });
-// });
-
-chrome.runtime.onMessage.addListener((msg, _, sendResponse) => {
-  console.log("TX intercepted:", msg);
-  sendResponse({ decision: "allow" });
-});
